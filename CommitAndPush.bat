54ca055510c5a2703ad5099152a15de23a1a5687
Dygo454 <diegoaguilar454@yahoo.com>
2024-07-12 21:43:18 +0900

    Implemented release build in batch file for easy use!

--- CommitAndPush.bat
+++ CommitAndPush.bat
@@ -1,8 +1,8 @@
 @echo off
 set /p "q=Would you like to compile(y/n): "
 if "%q%"=="" (set q=n)
-if "%q%"=="y" make
-if "%q%"=="Y" make
+if "%q%"=="y" make release
+if "%q%"=="Y" make release
 set /p "cm=Enter Commit Message: "
 if "%cm%"=="" (set cm=Unspecified Commit)
 make github-nocompile commit="%cm%"
